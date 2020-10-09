4e178da0a2bdb24e6bc49cce287c87c60e8e75f6
Supra-MS <suprabharathi.m.s@gmail.com>
2020-10-08 19:23:31 -0700

    Modify seeding script for Review photos

--- mongoDB/seed.js
+++ mongoDB/seed.js
@@ -18,7 +18,7 @@ const insertPictures = function () {
       await Pictures.insertMany(picData)
         .then(() => {
           console.log('Filled photos');
-          // cb();
+          cb();
         })
         .catch((err) => {
           console.log(err.reason);
@@ -37,27 +37,30 @@ const insertReviewPhotos = function () {
   let count = 0;
   async.whilst(
     () => count < 10,
-    (cb) => {
+    async (cb) => {
       count++;
       console.log('Review pic count: ', count);
-      let reviewPicData = generateReviewPhotosData(10000);
+      let reviewPicData = await generateReviewPhotosData(10000);
 
-      ReviewPhotos.create(reviewPicData)
+      await ReviewPhotos.insertMany(reviewPicData)
         .then(() => {
           console.log('Filled Review Photos');
           cb();
         })
         .catch((err) => {
-          console.log('Complete seeding 1 million records for Review Photos!!');
-          console.log('Complete iteration! expected - ', err.reason + '\n' + 'Press ctrl + c to exit');
+          console.log(err.reason);
         });
+    },
+    () => {
+      console.log('Complete seeding 1 million records for Review Photos!!');
+      console.log('Complete iteration!' + '\n' + 'Press ctrl + c to exit');
     }
   );
 
 };
 
 insertPictures();
-// insertReviewPhotos();
+insertReviewPhotos();
 
 
 

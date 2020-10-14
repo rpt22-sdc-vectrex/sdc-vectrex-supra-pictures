598040709fbc51a9cf8ad3a7f25b7d152a15dc90
Supra-MS <suprabharathi.m.s@gmail.com>
2020-10-14 16:31:57 -0700

    updating s3 urls for rdbms

--- rdbms-sdc/helpers/generatePicturesData.js
+++ rdbms-sdc/helpers/generatePicturesData.js
@@ -1,4 +1,5 @@
 const { randomPicsLogic } = require('./randomPicsLogic');
+let counter = 1;
 
 const generatePicturesData = (noOfRecords) => {
 
@@ -6,6 +7,7 @@ const generatePicturesData = (noOfRecords) => {
 
   for (var i = 1; i < noOfRecords + 1; i++) {
     pictures.push({
+      "item_id": counter++,
       "item_pictures": randomPicsLogic(),
       "seller_picture":"https://picsum.photos/75/75",
       "store_picture":"https://picsum.photos/140/140"
